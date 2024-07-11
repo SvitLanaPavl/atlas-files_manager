38bfc60d162703306bb761c529c643eec598a32a
meisibley <mkong99@yahoo.com>
2024-07-11 10:08:49 -0500

    task3 - bodyParser should be in server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./routes/index');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 
 dotenv.config();
 
 const app = express();
+app.use(bodyParser.json());
 const port = process.env.PORT || 5001; // my mac is running something system-related on 5000
 
 app.use('/', routes);
