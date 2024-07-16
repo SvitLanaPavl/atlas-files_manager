e679e9b3901c0f7c36ae71a51fb02a2cd8d687b0
meisibley <mkong99@yahoo.com>
2024-07-16 18:43:00 -0500

    task8, couldn't test

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -87,7 +87,7 @@ class FilesController {
     const fileId = new mongo.ObjectId(id);
     console.log(`FileId: ${fileId}`);
     const fileData = await DBClient.client.db().collection('files').findOne({ _id: fileId });
-    console.log(`File data: ${fileData}`);
+    console.log(`in getShow File data: ${fileData}`);
     if (!fileData) {
       return res.status(404).send({ error: 'Not found' });
     }
@@ -165,7 +165,7 @@ class FilesController {
       const fileId = new mongo.ObjectId(id);
       console.log(`FileId: ${fileId}`);
       const fileDocument = await DBClient.client.db().collection('files').findOne({ _id: fileId, userId: new mongo.ObjectId(userId)});
-      console.log(`File data: ${fileDocument}`);
+      console.log(`in putUnpublish File data: ${fileDocument}`);
       if (!fileDocument) {
         return res.status(404).json({ error: 'Not found'});
       }
@@ -175,6 +175,50 @@ class FilesController {
     } catch (err) {
       console.error(err);
       res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  static async getFile(req, res) {
+    console.log('in getFile');
+    const token = req.headers['x-token'];
+    const fileId = req.params.id;
+    console.log(`token: ${token}. fileId: ${fileId}`);
+    const database = await DBClient.client.db();
+    const userCollection = database.collection('users');
+    const fileCollection = database.collection('files');
+
+    const mongoUserId = await RedisClient.get(`auth_${token}`);
+    const user = await userCollection.findOne({ _id: new mongo.ObjectId(mongoUserId) });
+
+    if (!token) return res.status(404).json({ error:'Not found' });
+    if (!user) {
+      res.status(401).json({ error:'Unauthorized' });
+    } else {
+      console.log('user is authorized');
+      const filterQuery = { _id: new mongo.ObjectId(fileId) };
+      const file = await fileCollection.findOne(filterQuery);
+      console.log(`filterQuery: ${filterQuery}`);
+      console.log(`file: ${file}`);
+      if (!file) return res.status(404).json({ error:'Not found' });
+      if (!file.isPublic && file.userId.toString() !== user._id.toString()) {
+        return res.status(404).json({ error:'Not found' });
+      } else {
+        if (file.type === 'folder') {
+          res.status(400).json({error: "A folder doesn't have content"});
+        } else {
+          const mimeType = contentType(file.name) || 'text/plain';
+          const filePath = file.localPath;
+
+          fs.readFile(filePath, 'utf-8', (err, fileContent) => {
+            if (err) {
+              res.status(400).json({ error: 'Unable to read contents of the file'});
+            } else {
+            res.header('Content-Type', mimeType);
+            res.status(200).send(fileContent);
+            }
+          });
+        }
+      }
     }
   }
 }
