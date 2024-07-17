ce8de583469c21a4a6391898d8b8e7acbdd22840
meisibley <mkong99@yahoo.com>
2024-07-17 12:10:52 -0500

    task9 worker server doesn't work yet

--- worker.js
+++ worker.js
@@ -4,3 +4,24 @@ const DBClient = require('./utils/db');
 const mongo = require('mongodb');
 
 const fileQueue = new Bull('fileQueue');
+console.log('in worker.js');
+
+fileQueue.process(async (job) => {
+    console.log('in fileQueue process');
+    const { fileId, userId } = job.data;
+
+    if (!fileId) throw new Error('Missing fileId');
+    if (!userId) throw new Error('Missing userId');
+
+    const mongoFile = await DBClient.client.db().collection(files).findOne({ _id: mongo.ObjectId(fileId), userID });
+    if (!mongoFile) throw new Error('File not found');
+
+    const thumbnailWidth = [500, 250, 100];
+    for (const size of thumbnailWidth) {
+        const thumbnail = await imageThumbnail(mongoFile.localPath, { width: size, height: size });
+        const imageName = `${fileId}_${size}.jpg`;
+        await DBClient.client.db().collection('users').updateOne({ _id: mongo.ObjectId(userId) }, { $addToset: { [`${thumbnail}.${size}`]: imageName } });
+    }
+});
+
+module.exports = fileQueue;
